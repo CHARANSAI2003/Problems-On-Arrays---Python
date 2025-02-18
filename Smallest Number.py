47fe3f3e3823cd18e56d467a86c43797925ae39d
Charan Sai B <105934273+CHARANSAI2003@users.noreply.github.com>
2025-02-18 13:47:06 +0530

    Update Smallest Number.py

--- Smallest Number.py
+++ Smallest Number.py
@@ -1,8 +1,15 @@
 #Find the smallest number in an array
 
-n=list(map(int,input().split()))
+n=list(map(int,input().split(" ")))
 a=n[0]
 for i in n:
     if a>i:
         a=i 
 print(a)        
+
+----------------------------------------
+Input: 
+5 4 3 2 1
+
+Output: 
+1
